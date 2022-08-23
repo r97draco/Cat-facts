584da57ca8687253b69835555d998f181703d648
Dracø <sjagroop212@gmail.com>
2022-08-23 04:02:09 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function getInfo(event){
+    var elements = event.currentTarget;
+    var name= elements[0].value;
+  
+    console.log("Name: ", name);
+    event.preventDefault();
+  
+    var request = new XMLHttpRequest();
+    var api = 'https://meowfacts.herokuapp.com/';
+    console.log(api);
+    request.open('GET', api);
+    request.send();
+    
+    request.onload = ()=>{
+      console.log(request.status);
+      
+      // console.log(request.responseText);
+      var div= document.getElementById("data");
+      var result = JSON.parse(request.responseText);
+      
+      // console.log(result.data);
+      
+      var text=document.createTextNode(result.data);
+      var para = document.createElement('p');
+      para.append(text);
+      div.append(para);
+      div.insertBefore(para, div.firstChild);
+    }
+  }
+  
+  document.getElementById("getInfo").addEventListener("submit", getInfo, false);
